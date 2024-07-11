c66f7bdeda5052bdf9922a8902fd0373c2e3e585
vikram singh <vikram8905662530@gmail.com>
2024-07-11 18:07:48 +0530

    Add files via upload

---
+++ employee.py
@@ -0,0 +1,36 @@
+# Rohit
+import manager
+
+print(manager.rohit_id)
+print(manager.rohit_pass)
+
+print(manager.employee_list)
+
+#calling
+manager.rohit_hii()
+
+import manager as mang
+
+print(mang.rohit_id)
+print(mang.rohit_pass)
+
+print(mang.employee_list)
+
+#calling
+mang.rohit_hii()
+print(mang.manager_name)
+print(mang.manager_id)
+print(mang.manager_pass)
+
+
+from manager import rohit_id,rohit_pass
+from manager import rohit_hii
+from manager import manager_id
+
+
+print("employee file exucuted")
+print(rohit_id)
+print(rohit_pass)
+print()
+rohit_hii()
+print(manager_id)
