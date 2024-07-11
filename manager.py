9d8c4d06cb4a1159cd9284e88b147eac0ce76b01
vikram singh <vikram8905662530@gmail.com>
2024-07-11 18:08:36 +0530

    Add files via upload

---
+++ manager.py
@@ -0,0 +1,23 @@
+manager_name = "dharmpal ji"
+manager_address = "bihar"
+
+employee_list = ['rohit','vikram','sameeksha','vikash','shubham']
+rohit_id = 'hello@gmail.com'
+rohit_pass = '124@786'
+
+def rohit_hii() :
+    print("Employee_name : ","rohit")
+    print("rohit_id : ",rohit_id)
+    print("rohit_pass : ",rohit_pass)
+
+print("manager file executed!")
+if __name__ == "__main__" : # restiction 
+ print("manager file itself executed")
+ def manager_hii() :
+    print("Manager_name : ","dharmpal")
+    print("Manager_address : ",manager_address)
+    print('Dharmpal id : ',manager_id)
+    print('Dharmpal password : ',manager_pass)
+
+ manager_id = 'manager_bass@gmail.com'
+ manager_pass = '123@456'
